28f0a87b8e26692a1b7b44d201727ddd5fbda152
SanDevil23 <sankalpadhya05@gmail.com>
2026-05-07 01:01:48 +0530

    ingest handler created

--- services/ingest/internal/handler/ingest.go
+++ services/ingest/internal/handler/ingest.go
@@ -1,1 +1,22 @@
 package handler
+
+import (
+	"log/slog"
+
+	"github.com/nats-io/nats.go/jetstream"
+)
+
+// Ingest Handler handles incoming telemetry and publishes to NATS JetStream.
+type IngestHandler struct { 
+	js			jetstream.JetStream
+	streamName 	string
+	log 		*slog.Logger
+}
+
+func NewIngestHanlder(js jetstream.JetStream, strmName string, logger *slog.Logger) *IngestHandler {
+	return &IngestHandler{
+		js: js,
+		streamName: strmName,
+		log: logger,
+	}
+}
