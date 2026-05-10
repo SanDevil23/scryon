f8db392679fb9590abea172d48d3df695070e7f0
SanDevil23 <sankalpadhya05@gmail.com>
2026-05-10 16:15:10 +0530

    customer logger package with slog implemented

---
+++ pkg/logger/logger.go
@@ -0,0 +1,40 @@
+package logger
+
+import (
+	"context"
+	"log/slog"
+	"os"
+)
+
+
+type contextKey string
+
+const loggerKey contextKey = "logger"
+
+func New(level string) *slog.Logger {
+	var logLevel slog.Level
+	if err:=logLevel.UnmarshalText([]byte(level)); err!=nil {
+		logLevel = slog.LevelInfo
+	}
+
+	return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: logLevel,
+		AddSource: logLevel == slog.LevelDebug,
+	}))
+}
+
+func WithContext(ctx context.Context, log *slog.Logger) context.Context {
+	return context.WithValue(ctx, loggerKey, log)
+}
+
+func FromContext(ctx context.Context) *slog.Logger {
+	// safe type assertion
+	if log, ok := ctx.Value(loggerKey).(*slog.Logger); ok {
+		return log
+	}
+	return New("info")
+}
+
+func With(log *slog.Logger, args ...any) *slog.Logger {
+	return log.With(args...)
+}
