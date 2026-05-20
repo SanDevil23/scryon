f016b1a272b8882f3b9c8accc68af3849d47c18b
SanDevil23 <sankalpadhya05@gmail.com>
2026-05-20 23:43:10 +0530

    update consumer

--- services/processor/internal/consumer/consumer.go
+++ services/processor/internal/consumer/consumer.go
@@ -1,6 +1,7 @@
 package consumer
 
 import (
+	"context"
 	"log/slog"
 
 	"github.com/nats-io/nats.go/jetstream"
@@ -11,7 +12,31 @@ import (
 // to the appropriate backend writer.
 type Consumer struct {
 	jets 		jetstream.JetStream
-	victoria	*writer.VictoriaWriter
+	vWriter		*writer.VictoriaWriter
 	log			*slog.Logger
 	workers 	int
+}
+
+
+func New(js jetstream.JetStream, wr *writer.VictoriaWriter, log *slog.Logger, workers int) *Consumer {
+	return &Consumer {
+		jets: 			js,
+		vWriter:		wr,
+		log: 			log,
+		workers: 		workers,
+	}
+}
+
+// process routes a message to the correct writer based on subject.
+func (c *Consumer) process(ctx context.Context, msg jetstream.Msg) error {
+	sub := msg.Subject()
+	c.log.Debug("processing message", "subject", sub)
+
+	switch{
+	case len(sub) > 19 && sub[:19] == "telemetry.metrics.":
+		return c.handleMetrics(ctx, msg.Data())
+	default:
+		c.log.Debug("no handler for subject, skipping", "subject", sub)
+		return nil
+	}
 }
