f4f6c7fa6ad7bd6df1dbe950e353029291cba2cf
SanDevil23 <sankalpadhya05@gmail.com>
2026-07-26 11:45:03 +0530

    minor fix

--- services/processor/internal/writer/victoria_integration_test.go
+++ services/processor/internal/writer/victoria_integration_test.go
@@ -47,8 +47,8 @@ func setupVictoriaMetrics(t *testing.T) (*writer.VictoriaWriter, string, func())
 		t.Fatalf("failed to get container port: %v", err)
 	}
 
-	baseURL := fmt.Sprintf("http://%s:%s/", host, port.Port())
-	endpoint := baseURL + "api/v1/import/prometheus"
+	baseURL := fmt.Sprintf("http://%s:%s", host, port.Port())
+	endpoint := baseURL + "/api/v1/import/prometheus"
 
 	w := writer.NewVictoriaWriter(endpoint, noopLogger())
 
